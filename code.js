71a527a92a450f48930db873ebaf6ad406e1d1e1
bijuzgithub <bijunair1974@outlook.com>
2022-07-10 00:53:41 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    // alert("Hello");
+    // // console.log("Hello");
+    // // document.write("Hello");
+
+    // var number1=1;
+    // let number2=2;
+    // alert(number1/number2);
+    var itemPrice=310;
+    var userPayment=300;
+    if(userPayment==itemPrice){
+        alert("Payment Successfull")
+    }else{
+        alert("Not Sufficient Amount")
+    }  
+
+}
